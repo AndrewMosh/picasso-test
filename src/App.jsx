ba514947866fbb61549a0e8da9ed4c34dd0578ce
Andrew Mosh <andrewmosh@MacBook-Air-Andrew.local>
2023-11-26 15:53:24 +0300

    lazy loading added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 // App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import PostList from './components/PostList/PostList';
+import { Spinner } from '@chakra-ui/react';
 import PostDetail from './components/PostDetails/PostDetail';
 import Header from './components/Header/Header';
+const MyLazyPostList = React.lazy(() => import('./components/PostList/PostList'));
 const App = () => {
   return (
     <Router>
       <Header />
+      <React.Suspense fallback={<div style={{height: '80vh', display: 'flex', justifyContent: 'center', alignItems: 'center'}}><Spinner /></div>}>
       <Routes>
-        <Route path="/" element={<PostList />} />
+        <Route path="/" element={<MyLazyPostList />} />
         <Route path="/posts/:postId" element={<PostDetail />} />
       </Routes>
+      </React.Suspense>
     </Router>
   );
 };
