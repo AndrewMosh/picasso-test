bf0dc87bc2b8afc765f417c4eaa6acfd583fe678
Andrew Mosh <andrewmosh@MacBook-Air-Andrew.local>
2023-11-27 00:05:14 +0300

    Updates

---
+++ assets/Header-dSSOdhc9.js
@@ -0,0 +1,1 @@
+import{j as e,L as s}from"./index-rXNrS5Gu.js";const t="/picasso-test/assets/logo-HEdcpAo_.png",o="_header_cc42e_1",a={header:o},c=()=>e.jsx(s,{to:"/",children:e.jsx("header",{className:a.header,children:e.jsx("img",{src:t,alt:"blog",width:100,height:100})})});export{c as default};
